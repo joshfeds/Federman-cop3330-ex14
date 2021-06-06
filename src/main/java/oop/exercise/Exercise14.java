2602d5a4d06f5ce5e4c43cbff36019b0cf7cd1d2
joshfeds14 <joshfeds14@gmail,com>
2021-06-06 17:08:15 -0400

    Exercise 14

--- src/main/java/oop/exercise/Exercise14.java
+++ src/main/java/oop/exercise/Exercise14.java
@@ -1,17 +1,15 @@
 package oop.exercise;
+import java.text.NumberFormat;
 import java.util.Scanner;
+import java.util.Locale;
 /*
  *  UCF COP3330 Summer 2021 Assignment 1 Solution
  *  Copyright 2021 Joshua Federman
  */
-/*What is the order amount? 10
-        What is the state? WI
-        The subtotal is $10.00.
-        The tax is $0.55.
-        The total is $10.55.*/
 public class Exercise14
 {
     static Scanner input = new Scanner(System.in);
+    static final float STATE_TAX = 0.055F;
     public static String getOrder() {
         System.out.print("What is the order amount? ");
         return input.nextLine();
@@ -20,13 +18,29 @@ public class Exercise14
         System.out.print("What is the state? ");
         return input.nextLine();
     }
-    public static String getTax() {
-        System.out.print("What is the state? ");
-        return input.nextLine();
+    public static void getTotal(String a, String state){
+        int subtotal = Integer.parseInt(a);
+        float tax = subtotal * STATE_TAX;
+        float total = subtotal + tax;
+
+        Locale convert = new Locale("en", "US");
+        NumberFormat printDollar = NumberFormat.getCurrencyInstance(convert);
+
+        String taxState = "The total is " + printDollar.format(subtotal) + ".";
+
+        if(state.equals("WI"))
+        {
+            taxState = "The subtotal is " + printDollar.format(subtotal) +
+                    "\nThe tax is " + printDollar.format(tax) +
+                    "\nThe total is " + printDollar.format(total) + ".";
+        }
+
+        System.out.print(taxState);
+
     }
     public static void main( String[] args ) {
         String amount = getOrder();
         String state = getState();
-        String tax =
+        getTotal(amount, state);
     }
 }
